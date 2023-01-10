b90686dca29aeae713d96b1fe08dddd1da150753
Gudlarry <gudlarry@gmail.com>
2023-01-10 10:10:15 +0100

    first html with img

---
+++ index.js
@@ -0,0 +1,5 @@
+//var man = prompt('Enter your name');
+//var woman = prompt('Enter your girlfriend name');
+//let calc = Math.round(Math.random() * 100);
+//console.log(`the percentage of love ${man} has for ${woman} is ${calc}`);
+
